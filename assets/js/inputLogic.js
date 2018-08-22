85301b2c5edf9159be6bf4b8369cce83e3b06b68
root <root@MSI.localdomain>
2018-08-22 13:59:22 -0400

    adding game stat input and game stat tracking modal

--- assets/js/inputLogic.js
+++ assets/js/inputLogic.js
@@ -4,6 +4,16 @@ var newPlayerName = $('[name="name"]');
 var newPlayerDob = $('[name="dob"]');
 var newPlayerGrade = $('[name="grade"]');
 var newPlayerSports = $('[name="sports"]');
+var enterPlayerButton = $("#enter_player");
+
+var saveStatsButton = $("#saveStatsButton");
+
+
+saveStatsButton.click(function(){
+  setTimeout(function(){
+    $("#gameModal").modal('show');
+  }, 300);
+});
 
 
 function removeFromCurrentPlayersTable(element){
@@ -26,16 +36,28 @@ function bindLastRemoveButton(){
   });
 }
 
+enterPlayerButton.on("click", function(){
+  getPlayer();
+});
+
+function getPlayer(){
+  if(newPlayerName.val() != null && newPlayerName.val() !=""){
+    var toAdd = new Player(newPlayerName.val(), newPlayerDob.val(), newPlayerGrade.val(), newPlayerSports.val());
+    newPlayerName.val("");
+    newPlayerDob.val("");
+    newPlayerGrade.val("");
+    newPlayerSports.val("");
+    pushPlayer(toAdd);
+  }
+}
+
+
 
 $(document).on("keypress", function(event){
     if(event.which == 13 || event.keyCode == 13){
-        if(newPlayerName.val() != null && newPlayerName.val() !=""){
-          var toAdd = new Player(newPlayerName.val(), newPlayerDob.val(), newPlayerGrade.val(), newPlayerSports.val());
-          newPlayerName.val("");
-          newPlayerDob.val("");
-          newPlayerGrade.val("");
-          newPlayerSports.val("");
-          pushPlayer(toAdd);
-        }
+        getPlayer();
     }
 });
+
+//below will contain sorting methods for the players list, where players can be sorted by gradelevel, lexographical order in
+//terms of their name, birthdate, or whatever else someone asks for.
